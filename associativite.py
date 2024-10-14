f7c7b07295bb5ac40be561d5a0f270abc22e5cfe
Malvin Chevallier <malvin.chevallier@gmail.com>
2024-10-14 08:24:48 +0200

    python

---
+++ associativite.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+badResult = 0
+
+for i in range(0,50):
+    for j in range(0,50):
+        for k in range(0,50):
+            if (i*0.1+j*0.1)+k*0.1 != i*0.1+(j*0.1+k*0.1):
+                badResult += 1
+
+print("Manual range: Expected 26898; Current",badResult)
+
+badResult = 0
+
+for i in np.arange(0.0,5.0,0.1):
+    for j in np.arange(0.0,5.0,0.1):
+        for k in np.arange(0.0,5.0,0.1):
+            if (i+j)+k != i+(j+k):
+                badResult += 1
+
+print("Manual numpy arange: Expected 26898; Current",badResult)
