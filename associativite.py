e4151057b077ad0352b1b2328940e7c5e16930a2
Malvin Chevallier <malvin.chevallier@gmail.com>
2024-10-21 08:19:13 +0200

    add variant-1

--- associativite.py
+++ associativite.py
@@ -4,26 +4,26 @@ def main():
   badResult = 0
   cpt = 0
 
-  for i in range(0, 50):
-      for j in range(0, 50):
-          for k in range(0, 50):
+  for i in range(10, 60):
+      for j in range(10, 60):
+          for k in range(10, 60):
               cpt += 1
               if (i * 0.1 + j * 0.1) + k * 0.1 != i * 0.1 + (j * 0.1 + k * 0.1):
                   badResult += 1
 
-  print(f"Manual range: {(cpt-badResult)*100/cpt}%")
+  print(f"Manual range: {(cpt-badResult)*100/cpt}%; Operations {cpt}")
 
   badResult = 0
   cpt = 0
 
-  for i in np.arange(0.0, 5.0, 0.1):
-      for j in np.arange(0.0, 5.0, 0.1):
-          for k in np.arange(0.0, 5.0, 0.1):
+  for i in np.arange(1.0, 6.0, 0.1):
+      for j in np.arange(1.0, 6.0, 0.1):
+          for k in np.arange(1.0, 6.0, 0.1):
               cpt += 1
               if (i + j) + k != i + (j + k):
                   badResult += 1
 
-  print(f"Manual numpy arange: {(cpt-badResult)*100/cpt}%")
+  print(f"Manual numpy arange: {(cpt-badResult)*100/cpt}%; Operations {cpt}")
 
 if __name__ == "__main__":
   main()
