b60a2af69acd1f89fba0a3cb7aa664e4a6ee6540
Malvin Chevallier <malvin.chevallier@gmail.com>
2024-10-21 08:16:43 +0200

    percentage

--- associativite.py
+++ associativite.py
@@ -2,24 +2,28 @@ import numpy as np
 
 def main():
   badResult = 0
+  cpt = 0
 
   for i in range(0, 50):
       for j in range(0, 50):
           for k in range(0, 50):
+              cpt += 1
               if (i * 0.1 + j * 0.1) + k * 0.1 != i * 0.1 + (j * 0.1 + k * 0.1):
                   badResult += 1
 
-  print(f"Manual range: Expected 26898; Current {badResult}; Result: {26898 == badResult}")
+  print(f"Manual range: {(cpt-badResult)*100/cpt}%")
 
   badResult = 0
+  cpt = 0
 
   for i in np.arange(0.0, 5.0, 0.1):
       for j in np.arange(0.0, 5.0, 0.1):
           for k in np.arange(0.0, 5.0, 0.1):
+              cpt += 1
               if (i + j) + k != i + (j + k):
                   badResult += 1
 
-  print(f"Manual numpy arange: Expected 26898; Current {badResult}; Result: {26898 == badResult}")
+  print(f"Manual numpy arange: {(cpt-badResult)*100/cpt}%")
 
 if __name__ == "__main__":
   main()
